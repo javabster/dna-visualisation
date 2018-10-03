f446821a072cfb8ace22d3877c4a3097dbe24ab6
Abby Mitchell <abby.mitchell.15@ucl.ac.uk>
2018-10-03 14:51:41 +0100

    [S] Tidied up code a bit

--- dna.js
+++ dna.js
@@ -158,17 +158,19 @@ function addPebble(helix_id, pebble_description, helixColour, rock_id, rock_key,
     pathElem.setAttributeNS(null,"fill","transparent");
     document.getElementById(helix_id).appendChild(pathElem);
     
+    // text beneath helix (item + number)
     var fo = document.createElementNS(xmlns, 'foreignObject');
     fo.setAttribute('x', x1);
     fo.setAttribute('y', (y1*2)+3);
     fo.setAttribute('id', 'foreignObj' + rock_key);
     fo.innerHTML = pebble_description;
-    fo.setAttribute('height', 200);
-    fo.setAttribute('width', 250);
-    fo.setAttribute('style', 'display: flex; font-family: arial; color: transparent; animation: pebtxt' + 
+    fo.setAttribute('height', 100);
+    fo.setAttribute('width', 80);
+    fo.setAttribute('style', 'display: flex; text-align: center; font-family: arial; color: transparent; animation: pebtxt' + 
     num + '; animation-duration: 20s; animation-iteration-count: infinite');
     document.getElementById(helix_id).appendChild(fo);
 
+    // text inslide helix (% complete)
     var pcTxt = document.createElementNS(xmlns, 'foreignObject');
     pcTxt.setAttribute('x', x1-7);
     pcTxt.setAttribute('y', y1-15);
@@ -186,48 +188,48 @@ function addPebble(helix_id, pebble_description, helixColour, rock_id, rock_key,
 
 
 
-addRock('rocksHelix', 'Category 1', 'Rosalind', '#5b9aa0', 1, 7.777);
-addRock('rocksHelix', 'Category 2','Francis', '#034f84', 2, 22.458);
-addRock('rocksHelix', 'Category 3', 'James', '#b8a9c9', 3, 16);
-addRock('rocksHelix', 'Category 4', 'Maurice', '#622569', 4, 12.321);
+addRock('rocksHelix', 'Category1', 'Rosalind', '#5b9aa0', 1, 7.777);
+addRock('rocksHelix', 'Category2','Francis', '#034f84', 2, 22.458);
+addRock('rocksHelix', 'Category3', 'James', '#b8a9c9', 3, 16);
+addRock('rocksHelix', 'Category4', 'Maurice', '#622569', 4, 12.321);
 
-addPebble('Category 1', 'Item 1.1', '#5b9aa0', 1, 101, 20);
-addPebble('Category 1', 'Item 1.2', '#5b9aa0', 1, 102, 20);
-addPebble('Category 1', 'Item 1.3', '#5b9aa0', 1, 103, 50);
-addPebble('Category 1', 'Item 1.4', '#5b9aa0', 1, 104, 0);
-addPebble('Category 1', 'Item 1.5', '#5b9aa0', 1, 105, 0);
-addPebble('Category 1', 'Item 1.6', '#5b9aa0', 1, 106, 0);
-addPebble('Category 1', 'Item 1.7', '#5b9aa0', 1, 107, 0);
-addPebble('Category 1', 'Item 1.8', '#5b9aa0', 1, 108, 0);
-addPebble('Category 1', 'Item 1.9', '#5b9aa0', 1, 109, 0);
-addPebble('Category 1', 'Item 1.10', '#5b9aa0', 1, 110, 0);
+addPebble('Category1', 'Item 1.1', '#5b9aa0', 1, 101, 20);
+addPebble('Category1', 'Item 1.2', '#5b9aa0', 1, 102, 20);
+addPebble('Category1', 'Item 1.3', '#5b9aa0', 1, 103, 50);
+addPebble('Category1', 'Item 1.4', '#5b9aa0', 1, 104, 0);
+addPebble('Category1', 'Item 1.5', '#5b9aa0', 1, 105, 0);
+addPebble('Category1', 'Item 1.6', '#5b9aa0', 1, 106, 0);
+addPebble('Category1', 'Item 1.7', '#5b9aa0', 1, 107, 0);
+addPebble('Category1', 'Item 1.8', '#5b9aa0', 1, 108, 0);
+addPebble('Category1', 'Item 1.9', '#5b9aa0', 1, 109, 0);
+addPebble('Category1', 'Item 1.10', '#5b9aa0', 1, 110, 0);
 
 
-addPebble('Category 2', 'Item 2.1', '#034f84', 2, 201, 7.1);
-addPebble('Category 2', 'Item 2.2', '#034f84', 2, 202, 50);
-addPebble('Category 2', 'Item 2.3', '#034f84', 2, 203, 50);
-addPebble('Category 2', 'Item 2.4', '#034f84', 2, 204, 50);
-addPebble('Category 2', 'Item 2.5', '#034f84', 2, 205, 5);
-addPebble('Category 2', 'Item 2.6', '#034f84', 2, 206, 20);
-addPebble('Category 2', 'Item 2.7', '#034f84', 2, 207, 20);
-addPebble('Category 2', 'Item 2.8', '#034f84', 2, 208, 0);
-addPebble('Category 2', 'Item 2.9', '#034f84', 2, 209, 0);
+addPebble('Category2', 'Item 2.1', '#034f84', 2, 201, 7.1);
+addPebble('Category2', 'Item 2.2', '#034f84', 2, 202, 50);
+addPebble('Category2', 'Item 2.3', '#034f84', 2, 203, 50);
+addPebble('Category2', 'Item 2.4', '#034f84', 2, 204, 50);
+addPebble('Category2', 'Item 2.5', '#034f84', 2, 205, 5);
+addPebble('Category2', 'Item 2.6', '#034f84', 2, 206, 20);
+addPebble('Category2', 'Item 2.7', '#034f84', 2, 207, 20);
+addPebble('Category2', 'Item 2.8', '#034f84', 2, 208, 0);
+addPebble('Category2', 'Item 2.9', '#034f84', 2, 209, 0);
 
-addPebble('Category 3', 'Item 3.1', '#b8a9c9', 3, 301, 30);
-addPebble('Category 3', 'Item 3.2', '#b8a9c9', 3, 302, 30);
-addPebble('Category 3', 'Item 3.3', '#b8a9c9', 3, 303, 10);
-addPebble('Category 3', 'Item 3.4', '#b8a9c9', 3, 304, 0);
-addPebble('Category 3', 'Item 3.5', '#b8a9c9', 3, 305, 10);
+addPebble('Category3', 'Item 3.1', '#b8a9c9', 3, 301, 30);
+addPebble('Category3', 'Item 3.2', '#b8a9c9', 3, 302, 30);
+addPebble('Category3', 'Item 3.3', '#b8a9c9', 3, 303, 10);
+addPebble('Category3', 'Item 3.4', '#b8a9c9', 3, 304, 0);
+addPebble('Category3', 'Item 3.5', '#b8a9c9', 3, 305, 10);
 
-addPebble('Category 4', 'Item 4.1', '#622569', 4, 401, 25);
-addPebble('Category 4', 'Item 4.2', '#622569', 4, 402, 28.6);
-addPebble('Category 4', 'Item 4.3', '#622569', 4, 403, 0);
-addPebble('Category 4', 'Item 4.4', '#622569', 4, 404, 0);
-addPebble('Category 4', 'Item 4.5', '#622569', 4, 405, 0);
-addPebble('Category 4', 'Item 4.6', '#622569', 4, 406, 10.9);
-addPebble('Category 4', 'Item 4.7', '#622569', 4, 407, 50);
-addPebble('Category 4', 'Item 4.8', '#622569', 4, 408, 33.3);
-addPebble('Category 4', 'Item 4.9', '#622569', 4, 409, 0);
-addPebble('Category 4', 'Item 4.10', '#622569', 4, 410, 0);
-addPebble('Category 4', 'Item 4.11', '#622569', 4, 411, 0);
-addPebble('Category 4', 'Item 4.12', '#622569', 4, 412, 0);
+addPebble('Category4', 'Item 4.1', '#622569', 4, 401, 25);
+addPebble('Category4', 'Item 4.2', '#622569', 4, 402, 28.6);
+addPebble('Category4', 'Item 4.3', '#622569', 4, 403, 0);
+addPebble('Category4', 'Item 4.4', '#622569', 4, 404, 0);
+addPebble('Category4', 'Item 4.5', '#622569', 4, 405, 0);
+addPebble('Category4', 'Item 4.6', '#622569', 4, 406, 10.9);
+addPebble('Category4', 'Item 4.7', '#622569', 4, 407, 50);
+addPebble('Category4', 'Item 4.8', '#622569', 4, 408, 33.3);
+addPebble('Category4', 'Item 4.9', '#622569', 4, 409, 0);
+addPebble('Category4', 'Item 4.10', '#622569', 4, 410, 0);
+addPebble('Category4', 'Item 4.11', '#622569', 4, 411, 0);
+addPebble('Category4', 'Item 4.12', '#622569', 4, 412, 0);
